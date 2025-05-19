34572185948fe3b0c8c8ad4cdacac8eb8dce0784
Sam Worley <sam.worley15@utexas.edu>
2025-05-19 07:06:55 -0500

    really adding dev notebook?

--- sootopolis_notebook.py
+++ sootopolis_notebook.py
@@ -1,6 +1,60 @@
-import altair as alt
-import polars as pl
+import marimo
 
+__generated_with = "0.13.10"
+app = marimo.App(width="medium")
+
+with app.setup:
+    # Initialization code that runs before all other cells
+    import marimo as mo
+    import polars as pl
+    import pandas as pd
+    import altair as alt
+    from sklearn.linear_model import LinearRegression
+    from vega_datasets import data
+
+
+@app.cell
+def _():
+    cars = pl.from_pandas( data.cars() )
+    cars
+    return (cars,)
+
+
+@app.cell
+def _(cars):
+    alt.Chart(cars).mark_circle().encode(
+        alt.Y("Miles_per_Gallon"),
+        alt.X("Displacement"),
+        alt.Color("Acceleration:Q")
+    )
+    return
+
+
+@app.cell
+def _(cars):
+    sample = cars.filter(pl.col("Miles_per_Gallon").is_not_null()).filter(pl.col("Horsepower").is_not_null() )
+    X = sample.select(['Displacement','Acceleration','Weight_in_lbs' ])
+    y = sample.select('Miles_per_Gallon')
+    lr = LinearRegression().fit(X,y)
+
+    data_to_plot = []
+    point_to_plot = X[47]
+    for regressor,coef in zip(
+        ['Intercept']+X.columns, 
+        lr.intercept_.tolist() + lr.coef_.tolist()[0],
+    ):
+        if regressor != 'Intercept':
+            value = coef * point_to_plot[regressor][0]
+        else:
+            value = coef
+
+        data_to_plot.append({'label':regressor, 'amount':value})
+
+    data_to_plot.append({'label':'End', 'amount':0.0})
+    return X, data_to_plot, lr, y
+
+
+@app.function
 def setup_data(model , point_to_predict, regressor_labels):
     '''
     
@@ -21,6 +75,15 @@ def setup_data(model , point_to_predict, regressor_labels):
     return pl.DataFrame(data_to_plot)
 
 
+@app.cell
+def _(data_to_plot):
+    source=pl.DataFrame(data_to_plot)
+    source
+    return (source,)
+
+
+@app.function
+# Define frequently referenced fields
 def plot_altair_waterfall(source) -> alt.Chart:
     amount = alt.datum.amount
     label = alt.datum.label
@@ -97,6 +160,72 @@ def plot_altair_waterfall(source) -> alt.Chart:
         height=450
     )
 
+
+@app.cell
+def _(source):
+    plot_altair_waterfall(source)
+    return
+
+
+@app.cell
+def _(X, y):
+    lr2 = LinearRegression(fit_intercept=True).fit(X,y)
+
+    s = X[12]
+
+    sourc2 = setup_data(lr2, s, X.columns)
+    plot_altair_waterfall(sourc2)
+    return
+
+
+@app.cell
+def _(cars):
+    testbase = alt.Chart(cars)
+
+    testCircles = testbase.mark_circle().encode(
+        alt.X("Acceleration").scale(zero=False),
+        alt.Y("Miles_per_Gallon")
+        ,color=alt.Color('Origin:N')
+        ,tooltip=[
+            'Origin',
+            'Name',
+            alt.Tooltip("Acceleration"),
+            alt.Tooltip('Miles_per_Gallon')
+        ]
+    )
+    moTestChart = mo.ui.altair_chart(testCircles)
+
+    return (moTestChart,)
+
+
+@app.cell
+def _(bottomChart, moTestChart):
+    mo.vstack([
+        moTestChart,
+        bottomChart
+    ])
+    return
+
+
+@app.cell
+def _(X, lr, moTestChart):
+    if len(moTestChart.value)==1:
+        X_sample = moTestChart.value.select(X.columns)
+        #y_pred = lr.predict(X_sample)
+        #sourc3 = setup_data(lr, X_sample, X.columns)
+        #bottomChart = plot_altair_waterfall(sourc3).properties(
+        #    height=200,title='linear regression equation for predicting miles per gallon'
+        #)
+        bottomChart = plot_waterfall(lr, X_sample, X.columns).properties(
+            height=200,title='linear regression equation for predicting miles per gallon'
+        )
+    else:
+        print(len(moTestChart.value))
+        bottomChart = alt.Chart()
+    return (bottomChart,)
+
+
+@app.function
 def plot_waterfall(model, point_to_predict, regressor_labels):
     source = setup_data(
         model=model,
@@ -104,3 +233,12 @@ def plot_waterfall(model, point_to_predict, regressor_labels):
         regressor_labels=regressor_labels
     )
     return plot_altair_waterfall(source)
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
